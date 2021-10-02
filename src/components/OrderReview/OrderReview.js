795b14e31af84303048254ba9eec66ce5a25a432
sharif764 <ss1884088@gmail.com>
2021-10-03 00:34:27 +0600

    j

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
+import Cart from '../Cart/Cart';
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const [cart] = useCart(products);
     return (
         <div>
-            <h2>This is Order Review</h2>
+            <h2>Total Producs {products.length}</h2>
+            <h2>Cart length : {cart.length}</h2>
+            <Cart cart={cart}></Cart>
         </div>
     );
 };
