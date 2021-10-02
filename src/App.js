bfddb57d8b4b6965ac1135043c72169b32968ff8
sharif764 <ss1884088@gmail.com>
2021-10-02 22:28:32 +0600

    J

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import Notfound from './components/NotFound/Notfound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/orderreview">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="*">
+            <Notfound></Notfound>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
